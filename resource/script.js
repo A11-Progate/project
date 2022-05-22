ed246d271008876940dd05460175941fc237fe67
Eric Pradana <ericprd_@outlook.com>
2022-05-22 22:23:20 +0700

    adjust header element

--- resource/script.js
+++ resource/script.js
@@ -5,8 +5,7 @@ window.addEventListener('scroll', () => {
     if (document.body.clientWidth > 720) {
         menu.classList.toggle('sticky', window.scrollY > 20)
     } else {
-        // menu.classList.remove('sticky')
-        menu.classList.toggle('fade-in', window.scrollY > 110)
+        menu.classList.toggle('fade-in', window.scrollY > 100)
     }
 })
 
