c406c089081ab700c7191bb429a1990c078bf47c
Eric Pradana <ericprd_@outlook.com>
2022-05-22 19:46:15 +0700

    click logo to back to top

--- resource/script.js
+++ resource/script.js
@@ -13,14 +13,23 @@ window.addEventListener('scroll', () => {
 const listMenu = document.querySelector('.hamb-menu');
 const hamb = document.querySelector('.icon');
 
+// when hamburger icon clicked show the menu
 const showMenu = () => {
     listMenu.classList.toggle('show');
 }
 
+// remove menu list when clicked anywhere in main
 const mainBody = document.querySelector('main')
 
 mainBody.addEventListener('click', () => {
     if (listMenu.classList.contains('show')) {
-        listMenu.classList.remove('show')
+        listMenu.classList.remove('show');
     }
-})
+});
+
+// when logo clicked bring the view to the top
+const logo = document.querySelector('h3');
+
+logo.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+});
